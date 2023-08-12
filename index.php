2663b0366da2fc92a68e409fc974ec93e9d31565
Matheus Araújo <100092310+MtsAraujo@users.noreply.github.com>
2023-08-11 23:21:44 -0300

    visualização do modal

--- index.php
+++ index.php
@@ -11,24 +11,20 @@
         <h2 class='text-2xl'>Razao 5</h2>
         <form id="uploadForm" onsubmit="return ProcessaSalvamentoArquivos();">
             <label for="file">Escolha ou arraste um arquivo:</label>
-            <input type="file" name="file" id="file">
+            <input required type="file" name="file" id="file">
             <input type="hidden" value="5" name="razao_id">
-            <input class='relative mt-4 ml-1 w-28 px-1 py-2 text-xs font-medium text-white rounded-lg bg-primary' type="submit" value="Enviar">
+            <input class='relative mt-4 ml-1 w-28 px-1 py-2 cursor-pointer text-xs font-medium text-white rounded-lg bg-primary' type="submit" value="Enviar">
         </form>
         <br>
         <div id="message"></div>
 
-        <button idRazao='5' id='AbrirArquivos' onclick='ModalArquivos(this)' class='relative mt-4 ml-1 w-28 px-1 py-2 text-xs font-medium text-white rounded-lg bg-primary flex justify-center items-center'>
+        <bumtton idRazao='5' id='AbrirArquivos' onclick='ModalArquivos(this)' class='cursor-pointer relative mt-4 ml-1 w-28 px-1 py-2 text-xs font-edium text-white rounded-lg bg-primary flex justify-center items-center'>
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 12.75V12A2.25 2.25 0 014.5 9.75h15A2.25 2.25 0 0121.75 12v.75m-8.69-6.44l-2.12-2.12a1.5 1.5 0 00-1.061-.44H4.5A2.25 2.25 0 002.25 6v12a2.25 2.25 0 002.25 2.25h15A2.25 2.25 0 0021.75 18V9a2.25 2.25 0 00-2.25-2.25h-5.379a1.5 1.5 0 01-1.06-.44z" />
             </svg>
         </button>
-
     </div>
-</body>
-
-
-   <div class="ModalArquivos"></div>
+    <div class="ModalArquivos"></div>
 
 </body>
 </html>
--- upload.php
+++ upload.php
@@ -6,39 +6,70 @@ if (isset($_POST["idRazaoArquivo"])) {
 
         $idRazaoArquivo = $_POST["idRazaoArquivo"];
 
-        $sql = "SELECT * FROM arquivos";
+        $sql = "SELECT r.nome AS nome_razaosocial, a.nome AS nome_arquivo, a.path FROM arquivos a INNER JOIN razaosocial r ON a.id_razao = r.id WHERE a.id_razao = $idRazaoArquivo";
         $result = $con->query($sql);
 
         $popupArquivo = "";
-        $popupArquivo .= "
-        <div id='ArquivoModal' class='fixed inset-0 z-50 flex text-center items-center justify-center bg-gray-500 bg-opacity-100 overflow-auto'>
-            <div class='w-full max-w-2xl md:w-2/3 rounded-lg bg-white p-6 flex flex-col'>
-                <h2 class='text-xl font-bold'>Arquivos $idRazaoArquivo</h2>";
-
         if ($result->num_rows > 0) {
-            $popupArquivo .= "<div class='grid grid-cols-5 gap-4 mt-6'>";
-            
-            while ($row = $result->fetch_assoc()) {
-                $nome = $row['nome'];
-                $path = $row['path'];
-                
-                $popupArquivo .= "
-                <a class='md:w- ' target='_blank' href='$path'>
-                    <div class='flex flex-col rounded-lg items-center justify-center bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)] dark:bg-gray-100 sm:shrink-0 sm:grow sm:basis-0'>
-                        <div class='p-6'>
-                            <h5 class='text-xl font-medium leading-tight'>$nome</h5>
-                        </div>
-                        <img style='max-width: 80%;' class='rounded mb-4' src='$path' alt='$nome' /> 
+            $popupArquivo .= "
+            <div id='ArquivoModal' class='fixed w-full h-full inset-0 z-50 items-center justify-center bg-gray-500 bg-opacity-100 overflow-auto'>
+                <div class='mt-5 mb-5 mr-5 ml-5 rounded-lg bg-white p-6 '>    
+                    <div class='flex justify-between'>
+                        <h2 class='text-center text-2xl font-bold'>Nome da Prefeitura</h2>
+                        <button onclick='closeModalArquivo()' class='w-9 rounded bg-red-500 py-2 text-sm font-bold text-white hover:bg-red-700' type='button'>X</button>
                     </div>
-                </a>";
-            }
-            
-            $popupArquivo .= "</div>";
-        }
+                    <div class='grid grid-cols-5 gap-4 mt-6'>";
+
+            while ($row = $result->fetch_assoc()) {
+                $nomeArquivo = $row['nome_arquivo'];
+                $path = $row['path'];
+                $extensao = pathinfo($path, PATHINFO_EXTENSION);
+                $popupArquivo .= "
+                    <div class='mx-3 mt-6 flex-col self-start rounded-lg bg-slate-200 h-52'>
+                        <a class='mb-5' target='_blank'href='$path'>
+                            ";
+                            if (strtolower($extensao) === 'pdf') {
+                                $popupArquivo .= "
+                                                <div class='flex py-2 px-2 mt-2'>
+                                                    <img class='mx-3' src='./img-auxiliar/pdfIcon.png' alt='icon pdf'><h5 class='text-center text-xl font-medium leading-tight text-neutral-800 truncate'>$nomeArquivo</h5>
+                                                </div>
+                                                <div class='w-full h-full overflow-hidden'>
+                                                   <iframe class='object-none h-5/6 w-full p-6 object-fill' scrolling='no' src='$path' alt='$nomeArquivo'></iframe>
+                                                </div>";
+                            } else if (in_array(strtolower($extensao), ['png', 'jpeg', 'jpg'])) {
+                                $popupArquivo .= "
+                                                <div class='flex py-2 px-2 mt-2'>
+                                                    <img class='mx-3 ' src='./img-auxiliar/imgIcon.png' alt='icon img'><h5 class='text-center text-xl font-medium leading-tight text-neutral-800 truncate'>$nomeArquivo</h5>
+                                                </div>
+                                                <div class='w-full h-full overflow-hidden'>
+                                                    <img class='h-5/6 w-auto p-6 mx-auto' src='$path' alt='$nomeArquivo' />
+                                                </div>";
+                            }elseif (strtolower($extensao) === 'pptx'){
+                                $popupArquivo .= "
+                                                <div class='flex py-2 px-2 mt-2'>
+                                                    <img class='mx-3' src='./img-auxiliar/powerpointIcon.png' alt='icon powerpoint'><h5 class='text-center text-xl font-medium leading-tight text-neutral-800 truncate'>$nomeArquivo</h5>
+                                                </div>
+                                                <div class='w-full h-full overflow-hidden'>
+                                                    <img class='h-5/6 w-auto p-6 mx-auto' src='./img-auxiliar/powerpoint.jpg' alt='powerpoint' />
+                                                </div>";
+                            }elseif (strtolower($extensao) === 'xlsx'){
+                                $popupArquivo .= "
+                                                <div class='flex py-2 px-2 mt-2'>
+                                                    <img class='mx-3' src='./img-auxiliar/exelIcon.png' alt='icon exel'><h5 class='text-center text-xl font-medium leading-tight text-neutral-800 truncate'>$nomeArquivo</h5>
+                                                </div>
+                                                <div class='w-full h-full overflow-hidden'>
+                                                    <img class='h-5/6 w-auto p-6 mx-auto' src='./img-auxiliar/exel.png' alt='exel' />
+                                                </div>";
+                            }
+                            $popupArquivo .= "</a>
+                    </div>";
+            };
+        };
 
         $popupArquivo .= "
-                <button onclick='closeModalArquivo()' class='mt-4 self-center w-28 rounded bg-red-500 py-2 text-sm font-bold text-white hover:bg-red-700 md:w-1/5' type='button'>Fechar</button>
+                </div>
             </div>
+
         </div>";
 
         echo json_encode(array("view" => $popupArquivo), JSON_UNESCAPED_UNICODE);
@@ -58,8 +89,8 @@ if (isset($_FILES["file"]) && isset($_POST["razao_id"])) {
 
   if ($arquivo["size"] > 5000000) { // 5MB
       echo json_encode(array("message" => "Arquivo de no máximo 5MB"), JSON_UNESCAPED_UNICODE);
-  } else if (!in_array($extensao, array("jpg", "png", "jpeg", "pdf", "docx", "xls"))) {
-      echo json_encode(array("message" => "Formatos permitidos: JPG, PNG, JPEG, PDF, DOCX, XLS"), JSON_UNESCAPED_UNICODE);
+  } else if (!in_array($extensao, array("jpg", "png", "jpeg", "pdf", "docx", "xlsx","pptx"))) {
+      echo json_encode(array("message" => "Formatos permitidos: JPG, PNG, JPEG, PDF, DOCX, XLSX"), JSON_UNESCAPED_UNICODE);
   } else {
       $pasta = "arquivos/$razao/";
       $path = $pasta . $nomeArquivoUnico;
